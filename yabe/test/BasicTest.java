7de9457c657ee2c19a6e627ec1f6889f958d5f76
cuong.bui <cuongbm0211@gmail.com>
2017-05-23 15:33:36 +0700

    - Add Comment model then write unit test

--- yabe/test/BasicTest.java
+++ yabe/test/BasicTest.java
@@ -52,4 +52,35 @@ public class BasicTest extends UnitTest {
         assertNotNull(firstPost.postedAt);
     }
 
+    @Test
+    public void postComments() {
+        // Create new user and save it
+        User bob = new User("bob@gmail.com", "123", "Bob").save();
+
+        // Create new post
+        Post bobPost = new Post(bob, "My first post", "Hello world").save();
+
+        // Post a first comment
+        new Comment(bobPost, "Jeff", "Nice post").save();
+        new Comment(bobPost, "Tom", "I knew that!").save();
+
+        // Retrieve all comment
+        List<Comment> bobPostComments = Comment.find("byPost", bobPost).fetch();
+
+        // Tests
+        assertEquals(2, bobPostComments.size());
+
+        Comment firstComment = bobPostComments.get(0);
+        assertNotNull(firstComment);
+        assertEquals("Jeff", firstComment.author);
+        assertEquals("Nice post", firstComment.content);
+        assertNotNull(firstComment.postedAt);
+
+        Comment secondComment = bobPostComments.get(1);
+        assertNotNull(secondComment);
+        assertEquals("Tom", secondComment.author);
+        assertEquals("I knew that!", secondComment.content);
+        assertNotNull(secondComment.postedAt);
+    }
+
 }
