d56d86419e79b196e4f67cc0f29b7b2f1897948f
cuong.bui <cuongbm0211@gmail.com>
2017-05-23 15:22:10 +0700

    - Add Post model then write unit test

--- yabe/test/BasicTest.java
+++ yabe/test/BasicTest.java
@@ -5,6 +5,11 @@ import models.*;
 
 public class BasicTest extends UnitTest {
 
+    @Before
+    public void setup() {
+        Fixtures.deleteDatabase();
+    }
+
     @Test
     public void aVeryImportantThingToTest() {
         assertEquals(2, 1 + 1);
@@ -21,6 +26,30 @@ public class BasicTest extends UnitTest {
         // Test
         assertNotNull(cuong);
         assertEquals("Bui Manh Cuong", cuong.fullname);
+    }
+
+    @Test
+    public void createPost() {
+        // Create user and save it
+        User bob = new User("bob@gmail.com", "123", "Bob").save();
+
+        // Create a new post
+        new Post(bob, "My first post", "Hello world").save();
+
+        // Test the post has been created
+        assertEquals(1, Post.count());
+
+        // Retrieve all posts created by Bob
+        List<Post> bobPosts = Post.find("byAuthor", bob).fetch();
+
+        // Test
+        assertEquals(1, bobPosts.size());
+        Post firstPost = bobPosts.get(0);
+        assertNotNull(firstPost);
+        assertEquals(bob, firstPost.author);
+        assertEquals("My first post", firstPost.title);
+        assertEquals("Hello world", firstPost.content);
+        assertNotNull(firstPost.postedAt);
     }
 
 }
